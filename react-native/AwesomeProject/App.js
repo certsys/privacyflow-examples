4742d7497ec08a8563d32585de8a1d0bde31dbf1
Sergio Paulo Gomes <sergiopgomes92@gmail.com>
2022-03-08 11:32:05 -0300

    feat: :sparkles: add privacy links

--- react-native/AwesomeProject/App.js
+++ react-native/AwesomeProject/App.js
@@ -4,6 +4,10 @@ import RenderHtml from 'react-native-render-html';
 
 const source = {
   html: `
+<script src="https://privacyflow-hotfix.development.tec.br/api/v1/cookie_bars/token/62276367c37cdd001c99cdd2" async></script>
+<script src="https://privacyflow-hotfix.development.tec.br/api/v1/dsr/token/6227640bc37cdd001c99ce7a" async></script>
+
+<div id="implementationDSR">Id</div>
 <p style='text-align:center;'>
   Coloque o link do DSR e Barra de Cookies aqui.
 </p>`
