d1a2b1879bae612ced997862a59bc74f3c761ccb
Sergio Paulo Gomes <sergiopgomes92@gmail.com>
2022-03-08 11:16:30 -0300

    Add dsr and cookie bar to an angular app.

--- angularjs/app/dsr/dsr.js
+++ angularjs/app/dsr/dsr.js
@@ -10,5 +10,25 @@ angular.module('myApp.dsr', ['ngRoute'])
 }])
 
 .controller('DSRCtrl', [function() {
+  console.log('Start ----- JS do DSR');
+  
+  const script = document.createElement('script');
+  script.async = true;
+  script.type = 'text/javascript';                
+  script.src = 'https://privacyflow.development.tec.br/api/v1/dsr/token/620a4d8436a50e001d0f2ac0';
+  
+  document.head.appendChild(script);
+  console.log(script);
+  console.log('Finish ----- JS do DSR');
 
+  // const Scripts = [
+  //   {name: 'DSRScript', src: 'https://privacyflow.development.tec.br/api/v1/dsr/token/620a4d8436a50e001d0f2ac0'}
+  // ]
+
+  // Scripts.forEach((script) => {
+  //   this.scripts[script.name] = {
+  //     loaded: false,
+  //     src: script.src
+  //   };
+  // });
 }]);
