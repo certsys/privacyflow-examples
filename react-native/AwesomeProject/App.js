52a8dff828aa7f6fd44a58a0ba9d7ab0d056092e
Sergio Paulo Gomes <sergiopgomes92@gmail.com>
2022-03-09 10:22:52 -0300

    test

--- react-native/AwesomeProject/App.js
+++ react-native/AwesomeProject/App.js
@@ -1,12 +1,11 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, WebView } from 'react-native';
+import { WebView } from 'react-native-webview';
 import RenderHtml from 'react-native-render-html';
 
 const source = {
   html: `
-<script src="https://privacyflow-hotfix.development.tec.br/api/v1/cookie_bars/token/62276367c37cdd001c99cdd2" async></script>
-<script src="https://privacyflow-hotfix.development.tec.br/api/v1/dsr/token/6227640bc37cdd001c99ce7a" async></script>
-
 <div id="implementationDSR">Id</div>
 <p style='text-align:center;'>
   Coloque o link do DSR e Barra de Cookies aqui.
@@ -14,11 +13,32 @@ const source = {
 };
 
 export default function App() {
+  const [data, setData] = useState([]);
+
+  const getLinks = async () => {
+    try {
+     const response = await fetch('http://localhost:3333/api/v1/cookie_bars/token/62276367c37cdd001c99cdd2');
+     setData(response)
+     console.log(response)
+    //  const json = await response.json();
+    //  console.log(json)
+   } catch (error) {
+     console.error(error);
+   }
+  }
+
+  useEffect(() => {
+    getLinks();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Possui dados conosco?</Text>
       <StatusBar style="auto" />
       <RenderHtml source={source}/>
+      <RenderHtml source={{html: data}}/>
+      <WebView source={{ uri: 'http://localhost:3333/api/v1/cookie_bars/token/62276367c37cdd001c99cdd2' }} />
+      <WebView source={{ uri: 'http://localhost:3333/api/v1/dsr/token/6227640bc37cdd001c99ce7a' }} />
     </View>
   );
 }
